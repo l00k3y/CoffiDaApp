e9c5c65548c91654b52da761c966cc211fe9c589
Luke <lukebretherton@outlook.com>
2021-01-25 19:56:24 +0000

    2nd commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+class CoffiDaApp extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello Maaichael</Text>
+      </View>
+    );
+  }
+}
+
+export default CoffiDaApp;
